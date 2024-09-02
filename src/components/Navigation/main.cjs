6d5573aad0f01e5cded37083fc0c258f153bac4a
vatsal <rajvatsal2004@gmail.com>
2024-09-02 16:03:30 +0530

    refactor(navigation): Load css synchronously

--- src/components/Navigation/main.cjs
+++ src/components/Navigation/main.cjs
@@ -2,6 +2,6 @@ const html = require("./index.html").default;
 const scripts = require.context("./scripts/", false);
 
 module.exports = () => {
-	import("./styles.css");
+	require("./styles.css");
 	return { html, scripts };
 };
